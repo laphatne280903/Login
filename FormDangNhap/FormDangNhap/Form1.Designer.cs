7a7a715df93923113313947672df6ba48d1c7f94
laphatne280903 <phatnguyen280903@gmail.com>
2023-05-24 12:57:53 +0700

    Login

---
+++ FormDangNhap/FormDangNhap/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace FormDangNhap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.elipseControl1 = new ElipseToolDemo.ElipseControl();
+            this.elipseControl2 = new ElipseToolDemo.ElipseControl();
+            this.elipseControl4 = new ElipseToolDemo.ElipseControl();
+            this.elipseControl3 = new ElipseToolDemo.ElipseControl();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel_khung = new System.Windows.Forms.Panel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.comboBox_quyentruycap = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_DangNhap = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtb_mk = new System.Windows.Forms.TextBox();
+            this.txtb_tentk = new System.Windows.Forms.TextBox();
+            this.elipseControl5 = new ElipseToolDemo.ElipseControl();
+            this.label_Loi = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel_khung.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(280, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // elipseControl1
+            // 
+            this.elipseControl1.CornerRadius = 6;
+            // 
+            // elipseControl2
+            // 
+            this.elipseControl2.CornerRadius = 6;
+            // 
+            // elipseControl4
+            // 
+            this.elipseControl4.CornerRadius = 6;
+            this.elipseControl4.TargetControl = this.button_DangNhap;
+            // 
+            // elipseControl3
+            // 
+            this.elipseControl3.CornerRadius = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel3.Controls.Add(this.panel_khung);
+            this.panel3.Controls.Add(this.label_Loi);
+            this.panel3.Location = new System.Drawing.Point(201, 143);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(318, 310);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel_khung
+            // 
+            this.panel_khung.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_khung.BackColor = System.Drawing.Color.Transparent;
+            this.panel_khung.Controls.Add(this.linkLabel2);
+            this.panel_khung.Controls.Add(this.linkLabel1);
+            this.panel_khung.Controls.Add(this.comboBox_quyentruycap);
+            this.panel_khung.Controls.Add(this.label4);
+            this.panel_khung.Controls.Add(this.button_DangNhap);
+            this.panel_khung.Controls.Add(this.label2);
+            this.panel_khung.Controls.Add(this.label3);
+            this.panel_khung.Controls.Add(this.txtb_mk);
+            this.panel_khung.Controls.Add(this.txtb_tentk);
+            this.panel_khung.Location = new System.Drawing.Point(14, 3);
+            this.panel_khung.Name = "panel_khung";
+            this.panel_khung.Size = new System.Drawing.Size(290, 287);
+            this.panel_khung.TabIndex = 2;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.Cyan;
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.LinkColor = System.Drawing.Color.White;
+            this.linkLabel2.Location = new System.Drawing.Point(186, 256);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(56, 16);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Đăng ký";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Cyan;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(49, 256);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(103, 16);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Quên mật khẩu?";
+            // 
+            // comboBox_quyentruycap
+            // 
+            this.comboBox_quyentruycap.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox_quyentruycap.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox_quyentruycap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_quyentruycap.FormattingEnabled = true;
+            this.comboBox_quyentruycap.Items.AddRange(new object[] {
+            "ADMIN",
+            "GIẢNG VIÊN",
+            "SINH VIÊN"});
+            this.comboBox_quyentruycap.Location = new System.Drawing.Point(28, 39);
+            this.comboBox_quyentruycap.Name = "comboBox_quyentruycap";
+            this.comboBox_quyentruycap.Size = new System.Drawing.Size(233, 28);
+            this.comboBox_quyentruycap.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(35, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quyền truy cập";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button_DangNhap
+            // 
+            this.button_DangNhap.BackColor = System.Drawing.Color.DarkCyan;
+            this.button_DangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_DangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_DangNhap.ForeColor = System.Drawing.Color.White;
+            this.button_DangNhap.Location = new System.Drawing.Point(52, 203);
+            this.button_DangNhap.Name = "button_DangNhap";
+            this.button_DangNhap.Size = new System.Drawing.Size(190, 34);
+            this.button_DangNhap.TabIndex = 6;
+            this.button_DangNhap.Text = "Đăng nhập";
+            this.button_DangNhap.UseVisualStyleBackColor = false;
+            this.button_DangNhap.Click += new System.EventHandler(this.button_DangNhap_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(35, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên tài khoản";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(35, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mật khẩu";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtb_mk
+            // 
+            this.txtb_mk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb_mk.Location = new System.Drawing.Point(28, 163);
+            this.txtb_mk.Name = "txtb_mk";
+            this.txtb_mk.Size = new System.Drawing.Size(233, 26);
+            this.txtb_mk.TabIndex = 4;
+            this.txtb_mk.UseSystemPasswordChar = true;
+            // 
+            // txtb_tentk
+            // 
+            this.txtb_tentk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb_tentk.Location = new System.Drawing.Point(28, 101);
+            this.txtb_tentk.Name = "txtb_tentk";
+            this.txtb_tentk.Size = new System.Drawing.Size(233, 26);
+            this.txtb_tentk.TabIndex = 2;
+            // 
+            // elipseControl5
+            // 
+            this.elipseControl5.CornerRadius = 15;
+            this.elipseControl5.TargetControl = this.panel3;
+            // 
+            // label_Loi
+            // 
+            this.label_Loi.BackColor = System.Drawing.Color.LightCoral;
+            this.label_Loi.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_Loi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Loi.ForeColor = System.Drawing.Color.Black;
+            this.label_Loi.Location = new System.Drawing.Point(0, 0);
+            this.label_Loi.Name = "label_Loi";
+            this.label_Loi.Size = new System.Drawing.Size(318, 28);
+            this.label_Loi.TabIndex = 3;
+            this.label_Loi.Text = "Tên tài khoản hoặc mật khẩu không đúng!";
+            this.label_Loi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_Loi.Click += new System.EventHandler(this.label_Loi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(704, 491);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MANALO - Đăng nhập";
+            this.panel3.ResumeLayout(false);
+            this.panel_khung.ResumeLayout(false);
+            this.panel_khung.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private ElipseToolDemo.ElipseControl elipseControl1;
+        private ElipseToolDemo.ElipseControl elipseControl2;
+        private ElipseToolDemo.ElipseControl elipseControl4;
+        private ElipseToolDemo.ElipseControl elipseControl3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel_khung;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox comboBox_quyentruycap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_DangNhap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtb_mk;
+        private System.Windows.Forms.TextBox txtb_tentk;
+        private ElipseToolDemo.ElipseControl elipseControl5;
+        private System.Windows.Forms.Label label_Loi;
+    }
+}
+
---
+++ FormDangNhap/FormDangNhap/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormDangNhap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label_Loi.Hide();
+            
+        }
+        public static string Priority = string.Empty;
+        public static string Username=string.Empty;
+        private Form cformChild;
+        private void OpenChildForm(Form childForm)
+        {
+            if (cformChild != null)
+            {
+                cformChild.Close();
+            }
+            cformChild = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel_khung.Controls.Add(childForm);
+            panel_khung.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void button_DangNhap_Click(object sender, EventArgs e)
+        {
+            string quyentruycap = comboBox_quyentruycap.Text;
+            string tentk = txtb_tentk.Text;
+            string matkhau = txtb_mk.Text;
+
+            if (quyentruycap.Trim() == "") { panel_khung.Location = new Point(13, 28); label_Loi.Show(); }
+            else if (tentk.Trim() == "") { panel_khung.Location = new Point(13, 28); label_Loi.Show(); }
+            else if (matkhau.Trim() == "") { panel_khung.Location = new Point(13, 28); label_Loi.Show(); }
+
+
+            //điều kiện dưới đây check tài khoản đủ điều kiện đăng nhập hay không.
+            //else
+            //{
+
+            //}
+        }
+
+        private void label_Loi_Click(object sender, EventArgs e)
+        {
+            label_Loi.Hide();
+            panel_khung.Location = new Point(13, 3);
+        }
+    }
+}
